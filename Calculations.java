955369305e3fb47cff14bab6c566901684685738
Reem AlAmeeri <g00066638@aus.edu>
2020-02-04 11:26:06 +0400

    Issue 1 resolved

--- Calculations.java
+++ Calculations.java
@@ -23,12 +23,12 @@ public class Calculations {
 		
 		//Exercise 5: Spot the issues to be reported
 		addition(4,5); // should print the sum of the values provided
-	)
+	
 		welcome(); // should print the welcome message to the console
 	}
 	
 	public static int addition(int w, int z) {
-		int sum = w - z;
+		int sum = w + z;
 		return sum; 
 	}
 	
